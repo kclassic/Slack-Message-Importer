cef737252f7951841536bd5d906fee4cd30ea15f
KoodiVille <ville.salmela@storytel.com>
2018-06-11 22:19:06 +0200

    Changes FileImporter to use newton.

--- FileImporter.fs
+++ FileImporter.fs
@@ -1,37 +1,32 @@
 module FileImporter
-open FSharp.Data
-open FSharp.Data.JsonExtensions
 open Types
+open Newtonsoft.Json
+open System.IO
 
+let desereliazeFromFile<'a> path = 
+    path 
+    |> File.ReadAllText 
+    |> JsonConvert.DeserializeObject<'a>
+
+let getChannelName path =
+    path 
+    |> Path.GetDirectoryName 
+    |> (fun x -> x.Split(Path.DirectorySeparatorChar)) 
+    |> (fun x -> x.[x.Length - 1])
+    
 let getUserValues (path : string) = 
-    let users = JsonValue.Load(path)
-    let userArray = users.AsArray()
-
-    userArray
-    |> Array.map (fun x -> 
-        { Id = (x?id.AsString())
-          Name = (x?name.AsString())
-          DisplayName = (x?profile?display_name.AsString())
-          TimeZone = (x?tz.AsString()) })
-
-let getChannels (path : string) = 
-    let channelJson = JsonValue.Load(path)
-    let channels = channelJson.AsArray()
-
-    channels
-    |> Array.map (fun x -> 
-        { Id = (x?id.AsString()) 
-          Name = (x?name.AsString())
-          Members = (x?members.AsArray() |> Array.map (fun y -> y.AsString()))
-          Topic = { Creator =" "; Value = (x?topic?value.AsString()); LastSet =0.0 }
-          Purpose = { Creator = " "; Value = (x?purpose?value.AsString()); LastSet = 0.0 }})
+    desereliazeFromFile<User list> path
 
 let getMessages (path : string) =
-    let messageJson = JsonValue.Load(path)
-    let messages = messageJson.AsArray()
+    let messages = desereliazeFromFile<JMessage list> path
+    let channelName = getChannelName path
 
     messages
-    |> Array.map (fun x ->
-        { UserId = (x?user.AsString())
-          Text = (x?text.AsString())
-          TimeStamp = (x?ts.AsFloat()) })
+    |> List.map(fun x ->
+        { ChannelName = channelName
+          Type = x.Type
+          User = x.User
+          Text = x.Text
+          TimeStamp = x.TimeStamp
+          Edited = x.Edited
+          ClientMsgId = x.ClientMsgId})
