f5063b5dea1e481c07b3e69c673ad13240818e61
KoodiVille <ville.salmela@storytel.com>
2018-06-11 22:19:36 +0200

    Indexes all messages from backup

--- Program.fs
+++ Program.fs
@@ -2,6 +2,39 @@
 open System.Net.Http
 open Indexer
 open Types
+open System.IO
+
+let exitCode = 0
+
+let mapAndIndexMessage client (users:User list) (message:JMessage)  =
+    let start = DateTime(1970,1,1,0,0,0,DateTimeKind.Utc) 
+    let displayName = 
+        users
+        |> List.filter (fun user -> user.Id = message.User) 
+        |> List.map (fun user -> user.DisplayName) 
+        |> List.tryHead
+
+    let userName = 
+        users
+        |> List.filter (fun user -> user.Id = message.User) 
+        |> List.map (fun user -> user.Name) 
+        |> List.tryHead
+
+    printfn "user = %s" message.User
+    printfn "text = %s" message.Text
+    { UserName = 
+        match userName with
+        | Some x -> x
+        | None -> ""
+      DisplayName = 
+        match displayName with
+        | Some x -> x
+        | None -> ""
+      ChannelName = message.ChannelName
+      TimeStamp =  start.AddSeconds(message.TimeStamp)
+      Text = message.Text }
+    |> index client
+    |> ignore
 
 [<EntryPoint>]
 let main argv  =
@@ -10,85 +43,26 @@ let main argv  =
         argv
         |> Array.head
 
-    printfn "%s" token
+    let files =  
+        DirectoryInfo(__SOURCE_DIRECTORY__ + "/TestData/").GetDirectories "*" 
+        |> Array.collect (fun x -> Directory.EnumerateFiles(x.FullName) |> Seq.toArray)
+        
+    files
+    |> Array.iter(fun x -> printfn "%s" x)
 
     let users = FileImporter.getUserValues (__SOURCE_DIRECTORY__ + "/TestData/users.json")
-    let channels = FileImporter.getChannels (__SOURCE_DIRECTORY__ + "/TestData/channels.json")
-    let someMessages = FileImporter.getMessages (__SOURCE_DIRECTORY__ + "/TestData/general/2017-12-15.json")
 
-    users
-    |> Array.iter (fun x -> 
-        printfn "| Id: %s | name: %s | displayname: %s | TimeZone: %s |"
-            x.Id x.Name x.DisplayName x.TimeZone)
-
-    printfn "%s%s" "==================" Environment.NewLine
-    printfn "%s%s" "==================" Environment.NewLine
-
-    channels
-    |> Array.iter (fun x ->
-        printfn "| id: %s | name: %s | purpose: %s | topic: %s |"
-            x.Id x.Name x.Purpose.Value x.Topic.Value)
-
-    printfn "%s%s" "==================" Environment.NewLine
-    printfn "%s%s" "==================" Environment.NewLine
-
-    someMessages
-    |> Array.iter (fun x -> 
-        printfn "user: %s, message: %s" x.UserId x.Text)
-    
-    let indexMessages = 
-        someMessages 
-        |> Array.map (fun x ->
-            let start = DateTime(1970,1,1,0,0,0,DateTimeKind.Utc) 
-            { UserName = 
-                (users 
-                |> Array.filter (fun y -> y.Id = x.UserId) 
-                |> Array.map (fun z -> z.Name) 
-                |> Array.head)
-              DisplayName = 
-                (users 
-                |> Array.filter (fun y -> y.Id = x.UserId) 
-                |> Array.map (fun z -> z.DisplayName) 
-                |> Array.head)
-
-              ChannelName = "general"
-              TimeStamp =  start.AddSeconds(float x.TimeStamp)
-              Text = x.Text })   
-
-    let client = new HttpClient()
-    
-    let messages = SlackImporter.getMessages client token |> Async.RunSynchronously
-    
     let client = getClient
     createIndex client |> ignore
 
-    messages 
-        |> List.iter (fun message ->
-            let start = DateTime(1970,1,1,0,0,0,DateTimeKind.Utc) 
-            let user = 
-                users
-                |> Array.filter (fun users -> users.Id = message.User) 
-                |> Array.map (fun users -> users.DisplayName) 
-                |> Array.tryHead
+    files 
+    |> Array.iter(FileImporter.getMessages 
+        >> List.iter (fun message -> do mapAndIndexMessage client users message))
 
-            printfn "user = %s" message.User
-            printfn "text = %s" message.Text
-            { UserName = 
-                match user with
-                | Some x -> x
-                | None -> ""
-              DisplayName = 
-                match user with
-                | Some x -> x
-                | None -> ""
-              ChannelName = message.ChannelName
-              TimeStamp =  start.AddSeconds(message.TimeStamp)
-              Text = message.Text }
-            |> index client
-            |> ignore)   
+    let httpClient = new HttpClient()
+    
+    let messages = SlackImporter.getMessages httpClient token |> Async.RunSynchronously
 
-    indexMessages
-    |> Array.iter(fun message -> do index client message |> ignore)
+    messages |> List.iter (fun message -> do mapAndIndexMessage client users message)   
 
- 
-    0
+    exitCode
